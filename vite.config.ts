8a0052151af6046960654000755163bd87dd9c31
Nathan Reis <nathanlemos.ec@gmail.com>
2024-07-04 19:40:50 -0300

    Deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
+        base: "/delfos-web/",
         plugins: [vue()],
         resolve: {
             alias: {
