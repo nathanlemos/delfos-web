84911c6342520a377172418118ee68c6cb418e37
Nate Reis <nathanlemos.ec@gmail.com>
2024-07-04 19:59:21 -0300

    Update vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,17 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig(() => {
     return {
         base: "/delfos-web/",
-        plugins: [vue()],
+        plugins: [vue(),
+        {
+            name: "configure-response-headers",
+            configureServer: (server) => {
+            server.middlewares.use((_req, res, next) => {
+                res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+                res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+                next();
+            });
+            },
+        },],
         resolve: {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url))
