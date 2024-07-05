1834941a4a788adb4a36be0203a09cd94ac33cb1
Nate Reis <nathanlemos.ec@gmail.com>
2024-07-05 10:26:03 -0300

    Logo and info

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,12 @@
       <span
         class="navbar-brand d-flex align-items-center px-4 px-lg-5 cursor-pointer"
       >
-        <h2 class="m-0 text-primary"><i class="fa fa-book me-3"></i>Delfos</h2>
+        <img
+          class="img-fluid position-absolute"
+          src="/img/logo-full.png"
+          alt=""
+          style="height: 40px"
+        />
       </span>
     </router-link>
     <button
@@ -20,37 +25,37 @@
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <div class="navbar-nav ms-auto p-4 p-lg-0">
         <router-link to="/">
-          <span class="nav-item nav-link active">Home</span>
+          <span class="nav-item nav-link active">Início</span>
         </router-link>
         <router-link to="/about"
-          ><span class="nav-item nav-link">About</span></router-link
+          ><span class="nav-item nav-link">Sobre</span></router-link
         >
         <router-link to="/courses"
-          ><span class="nav-item nav-link">Courses</span></router-link
+          ><span class="nav-item nav-link">Pós-graduação</span></router-link
         >
         <div class="nav-item dropdown">
           <span class="nav-link dropdown-toggle" data-bs-toggle="dropdown"
-            >Pages</span
+            >Institucional</span
           >
           <div class="dropdown-menu fade-down m-0">
             <router-link to="/team"
-              ><span class="dropdown-item">Our Team</span></router-link
+              ><span class="dropdown-item">Equipe</span></router-link
             >
             <router-link to="/testimonials"
-              ><span class="dropdown-item">Testimonial</span></router-link
+              ><span class="dropdown-item">Depoimentos</span></router-link
             >
             <router-link to="/404"
-              ><span class="dropdown-item">404 Page</span></router-link
+              ><span class="dropdown-item">Novidades</span></router-link
             >
           </div>
         </div>
         <router-link to="/contact"
-          ><span class="nav-item nav-link">Contact</span></router-link
+          ><span class="nav-item nav-link">Contato</span></router-link
         >
       </div>
       <router-link to="/about"
         ><span class="btn btn-primary py-4 px-lg-5 d-none d-lg-block"
-          >Join Now<i class="fa fa-arrow-right ms-3"></i></span
+          >Ambiente virtual<i class="fa fa-arrow-right ms-3"></i></span
       ></router-link>
     </div>
   </nav>
