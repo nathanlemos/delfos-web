66516d9c8592ebe4d8136c22854154a843766382
nathanlemos <nathanlemos@users.noreply.github.com>
2024-07-05 12:57:31 +0000

    deploy: 52e3a37f9e4d8b252476d92523bf44d4e22c2ba9

---
+++ assets/About-BwyWUkyu.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./Hero.vue_vue_type_script_setup_true_lang-BopEacSM.js";import{S as a,A as t}from"./About-BEkrKneL.js";import{T as r}from"./Team-CCfri8QF.js";import{d as s,c as m,a as e,F as n,o as c}from"./index-RuMdbQBH.js";const b=s({__name:"About",setup(_){return(p,i)=>(c(),m(n,null,[e(o,{title:"Sobre"}),e(a),e(t),e(r)],64))}});export{b as default};
---
+++ assets/Team-8y8dYgCJ.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./Hero.vue_vue_type_script_setup_true_lang-BopEacSM.js";import{T as t}from"./Team-CCfri8QF.js";import{d as o,c as r,a as e,F as s,o as m}from"./index-RuMdbQBH.js";const l=o({__name:"Team",setup(n){return(c,_)=>(m(),r(s,null,[e(a,{title:"Instrutores"}),e(t)],64))}});export{l as default};
