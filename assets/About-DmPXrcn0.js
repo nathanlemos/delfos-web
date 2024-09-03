cc68fb1d3bd06add2ab88c687ea67714f0431e81
nathanlemos <nathanlemos@users.noreply.github.com>
2024-09-03 00:54:18 +0000

    deploy: dc26e7dddf4b2f058c6e3076ca9bfb55e8a7066b

--- assets/About-DmPXrcn0.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./Hero.vue_vue_type_script_setup_true_lang-DBTOEbyu.js";import{S as a,A as t}from"./About-CEGGndAB.js";import{T as r}from"./Team-BkYeURpp.js";import{d as s,c as m,a as e,F as n,o as c}from"./index-BDSuJTcE.js";const b=s({__name:"About",setup(_){return(p,i)=>(c(),m(n,null,[e(o,{title:"Sobre"}),e(a),e(t),e(r)],64))}});export{b as default};
