e6d8b8ea318a635f6d2de9f6c7e803c904388d6f
Nate Reis <nathanlemos.ec@gmail.com>
2024-09-02 21:39:53 -0300

    Fix CNAME file and url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
     return {
-        base:  mode === 'production' ? "/" : "/",
+        base:  mode === 'production' ? "delfos-web" : "/",
         plugins: [vue(),
         {
             name: "configure-response-headers",
